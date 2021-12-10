9054775dcee44723f39983c961aae9db3451246f
unknown <andrei.dima@usm.ro>
2021-12-10 21:51:09 +0200

    a

--- resources/views/carti_scanate/index.blade.php
+++ resources/views/carti_scanate/index.blade.php
@@ -75,7 +75,7 @@
                                     <b>{{ $carte_scanata->nr_pagini ?? '' }}</b>
                                 </td>
                                 <td>
-                                    <b>{{ $carte_scanata->utilizator->id ?? '' }}</b>
+                                    <b>{{ $carte_scanata->utilizator->id ?? '' }} - {{ auth()->user()->id ?? '' }}</b>
                                 </td>
                                 <td class="d-flex justify-content-end">
                                     <a href="{{ $carte_scanata->path() }}"
