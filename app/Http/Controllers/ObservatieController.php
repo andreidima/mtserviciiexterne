0698e7317c99819566b824b36ce543e025899a09
unknown <andrei.dima@usm.ro>
2021-12-23 08:48:37 +0200

    a

--- app/Http/Controllers/ObservatieController.php
+++ app/Http/Controllers/ObservatieController.php
@@ -77,7 +77,7 @@ class ObservatieController extends Controller
 
                 // Storage::makeDirectory($cale);
                 Storage::makeDirectory('app/uploads');
-                Storage::makeDirectory('uploads');
+                Storage::disk('local')->makeDirectory('uploads');
                 dd(Storage::makeDirectory($cale), storage_path());
 
                 // Prelucrarea pozei si salvarea pe hard-disk
